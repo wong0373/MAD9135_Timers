b23074aedca4a231bf960b300e124ea4becf0836
ShingKwan Terry Wong <wong0373@algonquinlive.com>
2024-11-01 11:49:13 -0400

    first commit

---
+++ Timers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timers
+//
+//  Created by Terry Wong on 1/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timers/TimersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimersApp.swift
+//  Timers
+//
+//  Created by Terry Wong on 1/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
